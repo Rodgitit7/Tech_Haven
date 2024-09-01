322229881dce914ad9941dbfc97200156c3ddceb
David4988 <davidson4988@gmail.com>
2024-09-01 18:15:03 +0530

    Jason commit

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -9,6 +9,7 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import LoginForm from './LoginForm'
 
 import classes from './index.module.scss'
+import CreateAccountForm from '../create-account/CreateAccountForm'
 
 export default async function Login() {
   await getMeUser({
@@ -35,7 +36,7 @@ export default async function Login() {
             <h3>Welcome</h3> 
             <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30}/>
           </div>
-          <p>Please login here</p>
+          <p>Please enter details</p>
           <LoginForm />
         </div>
       </div>
