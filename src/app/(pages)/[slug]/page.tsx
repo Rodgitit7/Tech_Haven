1200561d744e8d19aaf9a82f7beaabf7b4aebc09
David4988 <davidson4988@gmail.com>
2024-09-05 23:23:32 +0530

    David Commit

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -13,6 +13,7 @@ import { Hero } from '../../_components/Hero'
 import { generateMeta } from '../../_utilities/generateMeta'
 
 import classes from './index.module.scss'
+import Categories from '../../_components/Categories'
 
 // Payload Cloud caches all files through Cloudflare, so we don't need Next.js to cache them as well
 // This means that we can turn off Next.js data caching and instead rely solely on the Cloudflare CDN
@@ -58,9 +59,14 @@ export default async function Page({ params: { slug = 'home' } }) {
   return (
     <React.Fragment>
       {slug === 'home' ? (
-        <Gutter>
-          <Hero {...hero} />
+        <section>
+        <Hero {...hero} />
+
+        <Gutter className={classes.home}>
+          <Categories categories={categories} />
+          {/* <Promotion /> */}
         </Gutter>
+      </section>
       ) : (
         <>
           <Hero {...hero} />
